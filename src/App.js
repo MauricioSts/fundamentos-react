aad557edd61c00f11f514b00674b3326a631dbbb
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2024-04-18 13:33:20 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from'./components/Pessoa';
+import Frase from './components/Frase'
+function App() {
+const nome = 'Maurizio'
+  return (
+    <div className="App">
+      <h1> TESTE CSS </h1>
+      <Frase/>
+      <Frase/>
+      <SayMyName nome = 'Maureco' />
+      <SayMyName nome = 'Mauricio' />
+      <SayMyName nome = {nome}/>
+      <Pessoa
+       nome= 'MauMau' idade= '21' profissao= 'programador' foto='https://via.placeholder.com/150'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SayMyName.js
@@ -0,0 +1,8 @@
+function SayMyName (props){
+return(
+    <div>
+        <p> ola {props.nome} tudo bom?</p>
+    </div>
+)
+}
+export default SayMyName
